c56e7730b4ac12077f79d1ad446aeae821671ead
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-29 21:20:53 -0500

    adds some codewars problems

---
+++ src/Codewars.java
@@ -0,0 +1,22 @@
+public class Codewars {
+
+    public static int quarterOf(int month) {
+        if (month >= 1 && month <= 3) {
+            return 1;
+        } else if (month >= 4 && month <= 6) {
+            return 2;
+        } else if (month >= 7 && month <= 9) {
+            return 3;
+        } else return 4;
+    }
+
+
+    public static int multiply(int num,int num2) {
+        return num * num2;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
